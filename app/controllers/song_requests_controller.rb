e2fbc119ea5907bf63d99ea495da88fb1abab912
Allison Muschs <allisonmuschs@gmail.com>
2020-03-03 14:07:27 +0100

    song request views new in progress

--- app/controllers/song_requests_controller.rb
+++ app/controllers/song_requests_controller.rb
@@ -15,7 +15,7 @@ class SongRequestsController < ApplicationController
     @song_request.event = @event
     @song_request.user = current_user
     if @song_request.save
-      redirect_to @song_request
+      redirect_to song_requests_path
     else
       render :new
     end
@@ -29,6 +29,6 @@ class SongRequestsController < ApplicationController
   private
 
   def song_request_params
-    params.require(@song_request).permit(:input)
+    params.require(@song_request).permit(:input, :event_id, :user_id)
   end
 end
