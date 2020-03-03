a8c895a9fbe8d89b4ac1b25b15c0cb387445f761
Allison Muschs <allisonmuschs@gmail.com>
2020-03-03 17:37:17 +0100

    bug fixes, comment ok and edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
-    resources :song_requests, except: [:destroy]
+    resources :song_requests, except: [:destroy, :update]
   end
 
-  resources :comments, only: [:new, :create]
-  resources :song_requests, only: [:destroy]
+  resources :song_requests, only: [:destroy, :update]
+
+  resources :song_requests do
+   resources :comments, only: [:new, :create]
+  end
+  resources :comments do
+    resources :comments, only: [:new, :create]
+  end
 
 end
