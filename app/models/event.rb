8a4b838489f34af6765673b4528b92cb823f4ac7
rhonda coons <rhonda.l.coons@gmail.com>
2020-03-04 12:04:24 +0100

    form for submitting audio

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :song_requests, dependent: :destroy
-
+    has_one_attached :song_file
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
   validates :artist, presence: true
--- app/models/song_request.rb
+++ app/models/song_request.rb
@@ -2,7 +2,7 @@ class SongRequest < ApplicationRecord
   belongs_to :user
   belongs_to :event
   has_many :comments, as: :commentable, dependent: :destroy
-  has_one_attatched :song_file
+  has_one_attached :song_file
 
   validates :input, presence: true, uniqueness: true
 
